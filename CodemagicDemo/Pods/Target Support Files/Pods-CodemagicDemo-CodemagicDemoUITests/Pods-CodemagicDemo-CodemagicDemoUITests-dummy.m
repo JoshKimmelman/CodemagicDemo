5c66f1061683360258ed0fe3c7025e0b1610a761
Josh Kimmelman <jkimmelman@williamhill.us>
2020-11-25 14:15:46 -0500

    installed pod files

---
+++ CodemagicDemo/Pods/Target Support Files/Pods-CodemagicDemo-CodemagicDemoUITests/Pods-CodemagicDemo-CodemagicDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodemagicDemo_CodemagicDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CodemagicDemo_CodemagicDemoUITests
+@end
